7cf61f654c9edf19a716f14eff483ceee3384970
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-05-30 23:07:25 +0700

    init : init-commit

---
+++ src/components/section/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="layout-container bg-[]">
+    <div class="layout-child">
+      <div
+        class="flex justify-between px-[20px] md:px-[100px] lg:px-[165px] items-center gap-[30px]"
+      >
+        sjsjsj
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style lang="scss" scoped></style>
