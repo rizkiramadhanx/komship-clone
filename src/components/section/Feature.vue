9625a0138160e7d753bca9511c7691fc66ebf904
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-06-09 16:09:10 +0700

    fix : fix feature

--- src/components/section/Feature.vue
+++ src/components/section/Feature.vue
@@ -33,34 +33,40 @@
         <div class="flex flex-col gap-y-4">
           <template v-for="(item, key) in dataUnggulan" :key="key">
             <div
-              class="flex flex-col gap-y-6"
+              class="flex md:flex-col items-center md:items-baseline md:justify-normal justify-between gap-y-6"
               @click="setToggleDataUnggulan(item.type as TDataUnggulanToggle)"
             >
+              <div class="md:hidden">
+                <img :src="item.image" class="h-[100px]" :alt="item.title" />
+              </div>
               <h2
-                :class="item.type === dataUnggulanToggle ? 'text-brand-orange' : ''"
+                :class="getClassTitle(item, key)"
                 class="text-base font-semibold cursor-pointer hover:text-brand-orange"
               >
                 {{ item.title }}
               </h2>
-              <p class="text-[14px] text-[#99999A]">{{ item.description }}</p>
+              <p class="text-[14px] text-[#99999A] hidden md:block">{{ item.description }}</p>
             </div>
+            <p class="text-[14px] text-[#99999A] md:hidden">{{ item.description }}</p>
           </template>
         </div>
-        <img
-          src="@/assets/svg/feature/order-massal.png"
-          v-if="dataUnggulanToggle === 'INPUT'"
-          alt="Order massal"
-        />
-        <img
-          src="@/assets/svg/feature/pantau.png"
-          v-if="dataUnggulanToggle === 'PANTAU'"
-          alt="Order massal"
-        />
-        <img
-          src="@/assets/svg/feature/tarik-data.png"
-          v-if="dataUnggulanToggle === 'TARIK'"
-          alt="Order massal"
-        />
+        <div class="hidden md:block">
+          <img
+            src="@/assets/svg/feature/order-massal.png"
+            v-if="dataUnggulanToggle === 'INPUT'"
+            alt="Order massal"
+          />
+          <img
+            src="@/assets/svg/feature/pantau.png"
+            v-if="dataUnggulanToggle === 'PANTAU'"
+            alt="Order massal"
+          />
+          <img
+            src="@/assets/svg/feature/tarik-data.png"
+            v-if="dataUnggulanToggle === 'TARIK'"
+            alt="Order massal"
+          />
+        </div>
       </div>
       <div
         class="-ml-4 grid mt-[50px] grid-cols-1 md:grid-cols-2 place-items-stretch"
@@ -68,20 +74,28 @@
       >
         <div class="flex flex-col gap-y-[13px]">
           <template v-for="(item, key) in dataTambahan" :key="key">
-            <div>
-              <div class="flex items-start gap-x-2">
+            <div class="px-[20px]">
+              <div
+                class="flex items-center md:items-start gap-x-2"
+                :class="key % 2 ? 'justify-end' : 'justify-start'"
+              >
                 <img :src="item.image" :alt="item.title" />
-                <div>
-                  <div class="mt-2">
-                    <h3 class="text-brand-orange">{{ item.title }}</h3>
-                    <p class="text-[#212529]">{{ item.description }}</p>
+                <div :class="key % 2 && 'order-first md:order-none'">
+                  <div class="mt-2 flex md:flex-col">
+                    <h3 class="text-brand-orange text-[18px] font-semibold">{{ item.title }}</h3>
+                    <p class="text-[#212529] hidden md:block">{{ item.description }}</p>
                   </div>
                 </div>
               </div>
+              <p class="text-[#212529] md:hidden">{{ item.description }}</p>
             </div>
           </template>
         </div>
-        <img src="@/assets/svg/feature/TambahanHero.png" class="h-[600px]" alt="Tambahan" />
+        <img
+          src="@/assets/svg/feature/TambahanHero.png"
+          class="hidden md:block h-[600px]"
+          alt="Tambahan"
+        />
       </div>
     </div>
   </section>
@@ -97,6 +111,13 @@ type TDataTambahan = {
 }[]
 
 type TDataUnggulanToggle = 'INPUT' | 'TARIK' | 'PANTAU'
+
+type TDataUnggulan = {
+  title: string
+  description: string
+  image: any
+  type: TDataUnggulanToggle
+}[]
 const tambahan1 = new URL('@/assets/svg/feature/tambahan-1.svg', import.meta.url)
 const tambahan2 = new URL('@/assets/svg/feature/tambahan-2.svg', import.meta.url)
 const tambahan3 = new URL('@/assets/svg/feature/tambahan-3.svg', import.meta.url)
@@ -107,24 +128,27 @@ export default {
     return {
       dataUnggulan: [
         {
+          image: tambahan1,
           title: 'Input Order Masal',
           description:
             'Anti ribet untuk kamu yang harus input orderan cukup banyak setiap hari, Gak perlu lagi input data manual satu persatu.',
           type: 'INPUT'
         },
         {
+          image: tambahan2,
           title: 'Tarik Data Otomatis dan Mudah',
           description:
             'Data di dashboard akan semakin mudah kamu olah dengan memanfaatkan menu download. Kamu tinggal pilih klasifikasi data yang dibutuhkan.',
           type: 'TARIK'
         },
         {
+          image: tambahan3,
           title: 'Pantau Kendala Pengiriman',
           description:
             'Kendala pengiriman akan makin mudah dan cepat teratasi karena terhubung langsung dengan ekspedisi terkait.',
           type: 'PANTAU'
         }
-      ],
+      ] as TDataUnggulan,
       dataTambahan: [
         {
           image: tambahan1,
@@ -153,7 +177,7 @@ export default {
     }
   },
   setup() {
-    const toggle = ref<Toggle>('TAMBAHAN')
+    const toggle = ref<Toggle>('UNGULAN')
     const dataUnggulanToggle = ref<TDataUnggulanToggle>('INPUT')
 
     const setToggle = (e: Toggle) => {
@@ -165,6 +189,17 @@ export default {
     }
 
     return { toggle, setToggle, setToggleDataUnggulan, dataUnggulanToggle }
+  },
+  methods: {
+    getClassTitle(item: any, key: number) {
+      const type = item.type === this.dataUnggulanToggle ? 'text-brand-orange' : ''
+      const order = key % 2 ? 'order-first' : ''
+
+      const combineClass = type + order
+
+      console.log
+      return combineClass
+    }
   }
 }
 </script>
